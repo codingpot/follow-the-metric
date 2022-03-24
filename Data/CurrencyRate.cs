a5da18dcf982e83d9af31dde18589e099262e804
snowmerak <snowmerak@outlook.com>
2022-03-24 17:46:12 +0900

    create model

---
+++ Data/CurrencyRate.cs
@@ -0,0 +1,95 @@
+﻿namespace follow_the_metric.Data
+{
+    public class CurrencyRate
+    {
+        string? apiKey = null;
+        Dictionary<string, double> currencyRates = new Dictionary<string, double>();
+        DateTime? date = null;
+        Dictionary<string, string> descToCurrencyCode = new Dictionary<string, string>();
+
+        public CurrencyRate()
+        {
+            if (apiKey == null)
+            {
+                apiKey = Environment.GetEnvironmentVariable("CURRENCY_API_KEY");
+                if (apiKey == null)
+                {
+                    return;
+                }
+                Update();
+            }
+        }
+
+        private void Update() 
+        {
+            var client = new HttpClient();
+            var getRateTask = client.GetFromJsonAsync<CurrencyRateResponse>($"https://api.currencyfreaks.com/latest?apikey={apiKey}");
+            getRateTask.Wait();
+            if (getRateTask.Result != null)
+            {
+                currencyRates = getRateTask.Result.rates!;
+                date = DateTime.Parse(getRateTask.Result.date!);
+            }
+
+            var getListTask = client.GetFromJsonAsync<CurrencyListResponse[]>($"https://api.currencyfreaks.com/supported-currencies");
+            getListTask.Wait();
+            if (getListTask.Result != null)
+            {
+                descToCurrencyCode = new Dictionary<string, string>();
+                foreach (var country in getListTask.Result)
+                {
+                    if (country != null) {
+                        descToCurrencyCode.Add($"{country.countryName} - {country.currencyName} ({country.currencyCode})", country.currencyCode!);
+                    }
+                }
+            }
+        }
+
+        public double GetPair(string first, string second)
+        {
+            if (date != null)
+            {
+                var now = DateTime.Now;
+                if (now - date >= TimeSpan.FromHours(12.0))
+                {
+                    Update();
+                }
+            }
+            if (currencyRates == null)
+            {
+                return 1.0;
+            }
+            try {
+                return currencyRates[descToCurrencyCode[second]] / currencyRates[descToCurrencyCode[first]];
+            } catch (KeyNotFoundException) {
+                return 1.0;
+            }
+        }
+
+        public string[] GetCurrencys()
+        {
+            var keys = descToCurrencyCode.Keys.ToArray();
+            Array.Sort(keys);
+            return keys;
+        }
+    }
+
+    public class CurrencyRateResponse
+    {
+        public string? date { get; set; }
+        public string? @base { get; set; }
+        public Dictionary<string, double>? rates { get; set; }
+    }
+
+    public class CurrencyListResponse
+    {
+        public string? currencyCode { get; set; }
+        public string? currencyName { get; set; }
+        public string? icon { get; set; }
+        public string? status { get; set; }
+        public string? available_in_historical_data_from { get; set; }
+        public string? available_in_historical_data_till { get; set; }
+        public string? countryCode { get; set; }
+        public string? countryName { get; set; }
+    }
+}
---
+++ Data/Units.cs
@@ -0,0 +1,71 @@
+﻿namespace follow_the_metric.Data
+{
+    public class Units
+    {
+        public Dictionary<string, Dictionary<string, double>> units = new Dictionary<string, Dictionary<string, double>>();
+
+        public Units() 
+        {
+            units = new Dictionary<string, Dictionary<string, double>>()
+            {
+                { "length",
+                    new Dictionary<string, double>()
+                    {
+                        { "cm", 30.48 },
+                        { "m", 0.3048 },
+                        { "in", 12 },
+                        { "ft", 1 },
+                        { "yd", 0.3333 },
+                        { "miles", 0.00019 },
+                    }
+                },
+                { "area", 
+                    new Dictionary<string, double>()
+                    {
+                        { "cm2", 929.03 },
+                        { "m2", 0.092903 },
+                        { "in2", 144 },
+                        { "ft2", 1 },
+                        { "h2", 0.000009 },
+                        { "평", 0.028109 },
+                    }
+                },
+                { "weight",
+                    new Dictionary<string, double>()
+                    {
+                        { "mg", 1000 },
+                        { "g", 1 },
+                        { "kg", 0.001 },
+                        { "ton", 0.000001 },
+                        { "oz", 0.03527 },
+                        { "lb", 0.0022 },
+                    }
+                },
+                { "volume",
+                    new Dictionary<string, double>()
+                    {
+                        { "cu.cm", 1000000 },
+                        { "cu.m", 1 },
+                        { "ℓ", 1000 },
+                        { "cu.in", 61027 },
+                        { "cu.ft", 35.3165 },
+                        { "gal", 264.186 },
+                    }
+                }
+            };
+        }
+
+        public double GetPair(string kind, string first, string second)
+        {
+            if (!units.ContainsKey(kind))
+            {
+                return 1;
+            }
+            if (!units[kind].ContainsKey(first) || !units[kind].ContainsKey(second))
+            {
+                return 1;
+            }
+            return units[kind][second] / units[kind][first];
+        }
+    }
+}
